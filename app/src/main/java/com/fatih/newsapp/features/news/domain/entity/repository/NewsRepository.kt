7bdca29ca8985bca0f0f856462ed5305fe151e91
Fatih <ahmdfatihin17@gmail.com>
2024-05-12 00:47:20 +0700

    feat: add news repository

---
+++ app/src/main/java/com/fatih/newsapp/features/news/domain/entity/repository/NewsRepository.kt
@@ -0,0 +1,12 @@
+package com.fatih.newsapp.features.news.domain.entity.repository
+
+import arrow.core.Either
+import com.fatih.newsapp.core.exception.NetworkError
+import com.fatih.newsapp.features.news.domain.entity.News
+
+interface NewsRepository {
+
+    suspend fun getNews(query: HashMap<String, Any>) : Either<NetworkError, News>
+
+    suspend fun getHeadlines(query: HashMap<String, Any>) : Either<NetworkError, News>
+}
