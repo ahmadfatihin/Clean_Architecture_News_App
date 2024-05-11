12829f85a25d8bfd83a73f046011ea3954be4a71
Fatih <ahmdfatihin17@gmail.com>
2024-05-12 00:40:47 +0700

    feat: create appmodule
    Env BASE URL and API KEY not provided and uplaoded to github after here, please create on your own

---
+++ app/src/main/java/com/fatih/newsapp/core/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.fatih.newsapp.core.di
+
+import com.fatih.newsapp.core.di.Env.API_KEY
+import com.fatih.newsapp.core.di.Env.BASE_URL
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    private val loggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private fun setUpApiKey(chain: Interceptor.Chain) = chain.proceed(
+        chain.request()
+            .newBuilder()
+            .url(chain.request().url
+                .newBuilder()
+                .addQueryParameter("apiKey", API_KEY)
+                .build())
+            .build()
+    )
+
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .addInterceptor { setUpApiKey(it) }
+        .build()
+
+
+
+
+}
