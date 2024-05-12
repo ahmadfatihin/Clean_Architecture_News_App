0e6523ca5e408d690d9c186e0fa2844969123811
Fatih <ahmdfatihin17@gmail.com>
2024-05-12 12:14:01 +0700

    feat: add drawables

---
+++ app/src/main/java/com/fatih/newsapp/core/ui/Dimension.kt
@@ -0,0 +1,21 @@
+package com.fatih.newsapp.core.ui
+
+object Dimensions {
+    const val vertical_padding = 5
+    const val horizontal_padding = 10
+    const val all_padding = 5
+
+    const val bottom_app_bar_height = 50
+    const val bottom_app_bar_padding = 0
+
+    const val header_icon_padding = 3
+    const val header_icon_size = 40
+    const val header_spacer_padding = 3
+
+    const val content_image_width = 300
+    const val content_image_height = 200
+
+    const val icon_button_size = 25
+
+    const val medium_padding = 8
+}
